06e93b3007a76f63f1a5a0a795a9ff39818e3efc
Simonas Sabaliauskas <sim.sabaliauskas@gmail.com>
2024-05-17 00:22:23 +0300

    Įkelti užbaigti projektai Uzduotis13 ir Uzduotis14.

---
+++ Uzduotis13/Uzduotis13.cs
@@ -0,0 +1,128 @@
+﻿namespace Paskaita02
+{
+    public class Uzduotis13
+    {
+        // 1. Sukurkite masyvą su dešimties augalų pavadinimais.
+        // 2. Atspausdinkite kiekvieną uždavinio augalą atskiroje eilutėje.
+        // 3. Atspausdinkite uždavinio kiekvieną augalą pradedant nuo paskutinio, ir baigiant pirmuoju. (atvirkščias ciklas).
+        // 4. Suskaičiuokite kiek uždavinio masyve yra žodžių trumpesnių nei 5 simboliai, ir kiek ilgesnių nei 7 simboliai. (du skaičiavimai)
+        // 5. Suskaičiuokite kiek uždavinio masyve yra žodžių ilgesnių nei 5 simboliai bet trumpesnių nei 10 simboliai. (tarp 5 ir 10 simbolių ilgio)
+
+        public static void Main(String[] args)
+        {
+            // 1. Create 10-elements long array for keeping plant names
+            string[] plantNames =
+            {
+                "Cannabis sativa",
+                "Picea jezoensis",
+                "Theobroma cacao",
+                "Coffea arabica",
+                "Banisteriopsis caapi",
+                "Psychotria viridis",
+                "Erythroxylum coca",
+                "Solanum lycopersicum",
+                "Cucumis sativus",
+                "Spinacia oleracea"
+            };
+
+            // 2. Print every plant name in its own line
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(plantNames[i]);
+            }
+
+            Console.WriteLine();
+
+            // 3. Print every plant name in its own line starting from the last
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(plantNames[i]);
+            }
+
+            Console.WriteLine();
+
+            // 4. Count and print how many words in array are shorter than 5 symbols and how many are longer than 7 symbols
+            //    I am interpretting that a WORD is a collection of LETTERS only.
+
+            int asciiCode;
+            int wordLength;
+            int shortWords = 0;
+            int longWords = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                // Formatting strings to uppercase for easier access
+                plantNames[i] = plantNames[i].ToUpper();
+                wordLength = 0;
+
+                // Iterate through string chars and identify words
+                for (int j = 0; j < plantNames[i].Length; j++)
+                {
+                    asciiCode = plantNames[i][j];
+
+                    if (asciiCode > 64 && asciiCode < 91)
+                    {
+                        wordLength++;
+                        if (j == plantNames[i].Length - 1)
+                        {
+                            if (wordLength < 5)
+                            {
+                                shortWords++;
+                            }
+                            else if (wordLength > 7)
+                            {
+                                longWords++;
+                            }
+                        }
+                    }
+                    else if (wordLength > 0 && wordLength < 5)
+                    {
+                        wordLength = 0;
+                        shortWords++;
+                    }
+                    else if (wordLength > 7)
+                    {
+                        wordLength = 0;
+                        longWords++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{shortWords} zodziai(-is/-iu) trumpesni nei 5 raides, {longWords} zodziai(-is/-iu) ilgesni nei 7 raides.");
+
+            // 5. Count and print how many words in array are 6 to 9 letters long
+
+            int words6to9 = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                wordLength = 0;
+
+                // Iterate through string chars and identify words
+                for (int j = 0; j < plantNames[i].Length; j++)
+                {
+                    asciiCode = plantNames[i][j];
+
+                    if (asciiCode > 64 && asciiCode < 91)
+                    {
+                        wordLength++;
+                        if (j == plantNames[i].Length - 1)
+                        {
+                            if (wordLength > 5 && wordLength < 10)
+                            {
+                                words6to9++;
+                            }
+                        }
+                    }
+                    else if (wordLength > 5 && wordLength < 10)
+                    {
+                        wordLength = 0;
+                        words6to9++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{words6to9} zodziai(-is/-iu) yra nuo 6 iki 9 simboliu ilgio.");
+        }
+    }
+}
---
+++ Uzduotis14/Uzduotis14.cs
@@ -0,0 +1,109 @@
+﻿namespace Paskaita02
+{
+    public class Uzduotis14
+    {
+        // 1. Sugeneruokite 300 atsitiktinių skaičių nuo 0 iki 300, atspausdinkite juos atskirtus tarpais ir suskaičiuokite kiek tarp jų yra didesnių už 150.
+        // Skaičiai didesni nei 275 turi būti atspausdinti skliausteliuose” [ ] “.-------
+        // 2. Vienoje eilutėje atspausdinkite visus skaičius nuo 1 iki 3000, kurie dalijasi iš 77 be liekanos.
+        // Skaičius atskirkite kableliais. Po paskutinio skaičiaus kablelio neturi būti.
+        // 3. Nupieškite kvadratą iš “*”, kurio kraštines sudaro 25“*”
+
+        public static void Main(String[] args)
+        {
+            // 1. Generate random numbers
+            Generate300RandomNumbers();
+            Console.WriteLine();
+
+            // 2. Print all numbers [1;3000], divisible by 77, separated by commas, without a comma after the last.
+            // This is a reusable method for positive integers (uint).
+            ListMultiplesTidily(77, 1, 3000);
+            Console.WriteLine();
+
+            // 3. Square 25*25 made of stars "*".
+            // Would be better to make from "* " in the developer's console.
+            DrawStarCube(25);
+            Console.WriteLine();
+        }
+
+        private static void Generate300RandomNumbers()
+        {
+            Random random = new Random();
+            int integer;
+            int largerThan150 = 0;
+
+            for (int i = 0; i < 300; i++)
+            {
+                integer = random.Next(0, 301);
+
+                if (integer > 150)
+                    largerThan150++;
+
+                if (integer > 275)
+                    Console.Write($"[{integer}] ");
+                else
+                    Console.Write(integer + " ");
+            }
+
+            Console.WriteLine($"\n\nSkaiciu, didesniu nei 150, yra {largerThan150}");
+        }
+
+        private static void ListMultiplesTidily(uint baseNumber, uint floor, uint ceiling)
+        {
+            if (floor >= ceiling)
+            {
+                Console.WriteLine("Floor cannot be larger than the ceiling");
+                return;
+            }
+            // How many numbers to print
+            uint printCount = (ceiling - floor + 1) / baseNumber;
+            if (printCount < 1)
+            {
+                Console.WriteLine("No multiples to display within the specified range");
+                return;
+            }
+
+            // Finding first number
+            uint firstNumber = 0;
+            for (uint i = floor; i <= ceiling; i++)
+            {
+                if (i % baseNumber == 0)
+                {
+                    firstNumber = i;
+                    break;
+                }
+            }
+
+            // After what number not to print ", "
+            uint lastNumber = firstNumber + (baseNumber * (printCount - 1));
+
+            // This number will be updated and printed out
+            uint currentNumber = firstNumber;
+
+            // Print all multiples of the base number in the specified range
+            for (uint i = 0; i < printCount; i++)
+            {
+                Console.Write(currentNumber);
+
+                if (currentNumber != lastNumber)
+                    Console.Write(", ");
+
+                currentNumber += baseNumber;
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void DrawStarCube(uint sideLength)
+        {
+            Console.WriteLine("Jei \"*\" uzimtu kvadrato forma tekste, cia butu kvadratas su 25 \"*\" krastine");
+            for (int i = 0; i < sideLength; i++)
+            {
+                for (int j = 0; j < sideLength; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
